13cc806fb9ea98ad06ca2765e4c56706cbd48ebf
Anagho okiroro Obruthe <anaghosamroyce@gmail.com>
2022-07-28 02:45:16 +0100

    Managing Global State

--- src/components/Feedbackitem.jsx
+++ src/components/Feedbackitem.jsx
@@ -1,26 +1,17 @@
 import React from 'react'
-import { useState } from 'react'
+// import { useState } from 'react'
 
-const Feedbackitem = () => {
+const Feedbackitem = ({ item }) => {
 
     // Set the state
-    const [rating, setRating] = useState(7)
-    const [text, setText] = useState('This is an example of a feedback item')
+    // const [rating, setRating] = useState(7)
+    // const [text, setText] = useState('This is an example of a feedback item')
 
-    const handleClick = () => {
-        setRating((prev) => {
-            console.log(prev)
-            return prev + 1
-        })
-    }
 
     return (
         <div className='card'>
-            <div className="num-display">{rating}</div>
-            <div className="text-display">
-                {text}
-            </div>
-            <button onClick={handleClick}>Click</button>
+            <div className="num-display">{item.rating}</div>
+            <div className="text-display">{item.text}</div>
         </div>
     )
 }
