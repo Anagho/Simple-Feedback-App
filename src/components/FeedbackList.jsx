5f36f282c44f1231dfb1ef583555c0ae46750341
Anagho okiroro Obruthe <anaghosamroyce@gmail.com>
2022-07-29 03:46:20 +0100

    Events & Prop Drilling

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import Feedbackitem from './Feedbackitem'
 
-const FeedbackList = ({ feedback }) => {
+const FeedbackList = ({ feedback, handleDelete}) => {
     if (!feedback || feedback.length === 0) {
         return <p>No Feedback Yet</p>
     }
     return (
         <div className='feedback-list'>
             {feedback.map((item) => (
-                <Feedbackitem key={item.id} item={item} />
+                <Feedbackitem key={item.id} item={item} handleDelete = {handleDelete} />
             ))}
         </div>
     )
--- src/components/Feedbackitem.jsx
+++ src/components/Feedbackitem.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import {FaTimes} from 'react-icons/fa'
 import Card from './shared/Card'
 import PropTypes from 'prop-types';
 
-const Feedbackitem = ({ item }) => {
+const Feedbackitem = ({ item, handleDelete }) => {
+
 
     return (
         <Card>
             <div className="num-display">{item.rating}</div>
+            <button onClick={() => handleDelete(item.id)} className="close">
+                <FaTimes color='purple' />
+            </button>
             <div className="text-display">{item.text}</div>
         </Card>
     )
